b45401af078c9175f6c35e6ea60d9baf06556444
mateus <mateusb501@gmail.com>
2025-08-19 22:38:55 -0300

    Criei entrada de dadosda carta 1.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,12 +23,30 @@ int main() {
 
     // ++++ carta 01 ++++++
     char esdado;
-    char codigo[10]; 
+    char codigo[5]; 
     char NomeDaCidade[50];
     int populacao; 
     float area;  
     float pib;
     int pontosTuristicos;
+
+    // Entrada de dados da carta 1
+    printf("Cadastro da carta 1:\n");
+    printf("Digite a inicial do estado entre (A - H): ");
+    scanf(" %c", &esdado);
+    printf("Digite o código da carta (Ex: A01): ");
+    scanf("%s", codigo);
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", NomeDaCidade);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao);
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%f", &area);
+    printf("Digite o PIB da cidade (em milhões): ");
+    scanf("%f", &pib);
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontosTuristicos);
+
 
     return 0;
 }
